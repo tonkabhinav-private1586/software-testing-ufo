45943e3de30fb660eba8e012ccb921fc8835f56f
Abhinav Tonk <testingselenium15@gmail.com>
2018-09-05 19:46:57 +0530

    Adding JSONFileReader

---
+++ src/main/java/com/abhinavtonk/ufo/datamanaging/JSONFileReader.java
@@ -0,0 +1,17 @@
+package com.abhinavtonk.ufo.datamanaging;
+
+public class JSONFileReader implements IDataManager{
+
+	@Override
+	public IDataManager getDataSourceHandle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <U, T> U getData(T... t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
